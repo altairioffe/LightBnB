7fe901ebca03311cf7c5253dc0c083624169c33c
Altair <altair.ioffe@gmail.com>
2020-03-05 05:03:29 +0000

    added 2nd seed file and 5 queries

---
+++ 1_queries/4_most_visited_cities.sql
@@ -0,0 +1,5 @@
+SELECT properties.city as city, count(reservations.*) as total_reservations
+FROM properties
+JOIN reservations ON properties.id = property_id
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
