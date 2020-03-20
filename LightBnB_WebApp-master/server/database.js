966498adb2232af4630752d50c8467723b53d8b3
Altair <altair.ioffe@gmail.com>
2020-03-20 06:58:07 +0000

    added properties query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -98,11 +98,49 @@ exports.getAllReservations = getAllReservations;
 
 
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT properties.* FROM properties
-  LIMIT $1
-  `, [limit])
+
+  const queryParams = [];
+  
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += `WHERE owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push([`%${options.minimum_price_per_night}%`, `%${options.maximum_price_per_night}%`]);
+    queryString += `WHERE cost_per_night BETWEEN $${queryParams.length[0]} AND $${queryParams.length[1]} `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`%${options.minimum_rating}%`);
+    queryString += `WHERE average_rating >= $${queryParams.length} `;
+  }
+  
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams)
   .then(res => res.rows);
+
+
 }
 
 exports.getAllProperties = getAllProperties;
